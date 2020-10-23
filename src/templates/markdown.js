86a15b7e2aa533ee9d729e5a3df5119433351eba
Matt Dunn <mattd@nulogy.com>
2020-10-23 10:24:28 -0400

    updates markdown styles to match NDS

--- src/templates/markdown.js
+++ src/templates/markdown.js
@@ -2,29 +2,60 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import { Helmet } from "react-helmet";
-import { Title, Box } from "@nulogy/components";
+import { Title, Box, theme } from "@nulogy/components";
 import styled from "styled-components";
-import * as tokens from "@nulogy/tokens";
 import { Intro, IntroText, Layout } from "../components";
 
-const MarkdownStyles = styled.div`
-  h1 {
-    font-weight: 100 !important;
+const MarkdownStyles = styled(Box)({
+  color: theme.colors.black,
+
+  h1: {
+    fontWeight: theme.fontWeights.light,
+    marginTop: 0,
+    marginBottom: theme.space.x6,
+    fontSize: theme.fontSizes.heading1,
+    lineHeight: theme.lineHeights.heading1,
+    fontWeight: theme.fontWeights.light
+  },
+
+  h2: {
+    marginTop: "24px",
+    marginBottom: theme.space.x1,
+    fontSize: theme.fontSizes.heading2,
+    lineHeight: theme.lineHeights.heading2,
+    fontWeight: theme.fontWeights.normal
+  },
+
+  h3: {
+    marginTop: "24px",
+    marginBottom: theme.space.x1,
+    fontSize: theme.fontSizes.heading3,
+    lineHeight: theme.lineHeights.heading3,
+    fontWeight: theme.fontWeights.medium
+  },
+
+  h4: {
+    marginTop: "24px",
+    marginBottom: theme.space.x1,
+    fontSize: theme.fontSizes.heading4,
+    lineHeight: theme.lineHeights.heading4,
+    fontWeight: theme.fontWeights.medium
+  },
+
+  "a, a:visited": {
+    color: theme.colors.blue
+  },
+
+  "a:hover": {
+    color: theme.colors.darkBlue
+  },
+
+  code: {
+    fontSize: theme.fontSizes.small,
+    color: theme.colors.darkGrey,
+    lineHeight: theme.lineHeights.smallTextBase
   }
-  h1,
-  h2,
-  h3,
-  h4 {
-    font-weight: 500;
-  }
-  a,
-  a:visited {
-    color: ${tokens.color_base_blue};
-  }
-  a:hover {
-    color: ${tokens.color_base_dark_blue};
-  }
-`;
+});
 
 export default function Template({
   data // this prop will be injected by the GraphQL query below.
