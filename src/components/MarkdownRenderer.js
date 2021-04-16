d4d7f7474d15b7f0faf7095e9e2425307785d57c
Matt Dunn <mattd@nulogy.com>
2021-04-16 13:01:31 -0400

    simplifies the design and layout a bit

--- src/components/MarkdownRenderer.js
+++ src/components/MarkdownRenderer.js
@@ -1,24 +1,23 @@
 import React from "react";
 import {MDXProvider} from "@mdx-js/react";
-import {Text, List, ListItem, Heading1, Heading2, Heading3} from "@nulogy/components";
+import {Text, Link, List, ListItem, Heading1, Heading2, Heading3} from "@nulogy/components";
 import CodeBlock from "./CodeBlock"
-import PropTable from "./PropTable"
 
-function MarkdownRenderer({ props, children, location }) {
-  console.log(children);
+function MarkdownRenderer({ children }) {
   return (
     <MDXProvider components={{
+      h1: (props) => <Heading1 margin="0 auto" {...props} />,
       h1: Heading1,
       h2: (props) => <Heading2 mt="x6" {...props} />,
-      h3: (props) => <Heading3 mt="x4" {...props} />,
-      p: (props) => <Text fontSize="20px" mb="x4" {...props} />,
+      h3: (props) => <Heading3  mt="x4" {...props} />,
+      p: (props) => <Text mb="x4" {...props} />,
       blockquote: (props) => <Text fontSize="24px" mb="x4" {...props} />,
       ul: (props) => <List mb="x4" {...props} />,
-      li: (props) => <ListItem fontSize="18px"  {...props} />,
+      li: (props) => <ListItem  {...props} />,
+      a: (props) => <Link {...props} />,
       pre: (props) => <CodeBlock {...props} />,
     }}>
       {children}
-      test
     </MDXProvider>
   );
 }
