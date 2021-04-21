d7388de8ff8a194c91681d34c14d331c489b696a
Matt Dunn <mattd@nulogy.com>
2021-04-21 15:03:37 -0400

    add breadcrumbs page

---
+++ src/playgrounds/breadcrumbs.js
@@ -0,0 +1,19 @@
+import { Breadcrumbs, Link, Text } from "@nulogy/components";
+import {PropTypes} from 'react-view';
+
+export const config = {
+  componentName: "Breadcrumbs",
+  scope: {
+    Breadcrumbs, 
+    Link,
+    Text
+  },
+  props: {
+    children: {
+      value: `<Link href="">Home</Link>
+<Link href="">Tenants</Link>`,
+      type: PropTypes.ReactNode,
+      description: "The elements to build the breadcrumbs out of. Can be a Link or Text."
+    },
+  }
+}
