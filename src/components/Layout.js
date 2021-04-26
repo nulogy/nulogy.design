c8e27bd93315dfae43b6f9b816bbbacbcf98f30a
Marianne Borowiak <marianneb@nulogy.com>
2021-04-26 14:58:38 -0400

    fix typo importing helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,49 +1,49 @@
 import React from 'react'
-import {Box, Flex, ApplicationFrame} from "@nulogy/components";
+import { Box, Flex, ApplicationFrame } from "@nulogy/components";
 import Navigation from './Navigation'
 import Transition from './Transition'
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 
 const StripedBorder = () => (
   <Flex height="16px">
-    <Box width={1/7} backgroundColor="black" />
-    <Box width={1/7} backgroundColor="blackBlue" />
-    <Box width={1/7} backgroundColor="darkBlue" />
-    <Box width={1/7} backgroundColor="blue" />
-    <Box width={1/7} backgroundColor="lightBlue" /> 
-    <Box width={1/7} backgroundColor="lightYellow" /> 
-    <Box width={1/7} backgroundColor="yellow" /> 
+    <Box width={1 / 7} backgroundColor="black" />
+    <Box width={1 / 7} backgroundColor="blackBlue" />
+    <Box width={1 / 7} backgroundColor="darkBlue" />
+    <Box width={1 / 7} backgroundColor="blue" />
+    <Box width={1 / 7} backgroundColor="lightBlue" />
+    <Box width={1 / 7} backgroundColor="lightYellow" />
+    <Box width={1 / 7} backgroundColor="yellow" />
   </Flex>
 )
 
 const Layout = ({ children, location }) => (
-    <ApplicationFrame>
-      <Helmet titleTemplate="%s | Nulogy Design System">
-        <html lang="en" />
-        <meta charSet="utf-8" />
-        <title>Welcome</title>
-        <link
-          href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:300,400,500,600"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono"
-          rel="stylesheet"
-        />
-      </Helmet>
-      <Flex>
-        <Box position="fixed" height="100%" overflow="hidden auto" width="320px" as="aside" px="x8" py="x8" bg="darkBlue">
-          <Navigation location={location} />
-        </Box>
-        <Box width="calc(100% - 320px)" marginLeft="320px">
+  <ApplicationFrame>
+    <Helmet titleTemplate="%s | Nulogy Design System">
+      <html lang="en" />
+      <meta charSet="utf-8" />
+      <title>Welcome</title>
+      <link
+        href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:300,400,500,600"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono"
+        rel="stylesheet"
+      />
+    </Helmet>
+    <Flex>
+      <Box position="fixed" height="100%" overflow="hidden auto" width="320px" as="aside" px="x8" py="x8" bg="darkBlue">
+        <Navigation location={location} />
+      </Box>
+      <Box width="calc(100% - 320px)" marginLeft="320px">
         <Transition location={location}>
-            <Box as="main" paddingTop="x6" paddingX="x8">
-              <Box mb="x6">{children}</Box>
-            </Box>
+          <Box as="main" paddingTop="x6" paddingX="x8">
+            <Box mb="x6">{children}</Box>
+          </Box>
         </Transition>
-        </Box>
-      </Flex>
-    </ApplicationFrame>
+      </Box>
+    </Flex>
+  </ApplicationFrame>
 )
 
 export default Layout
