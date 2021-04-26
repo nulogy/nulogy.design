c3c321f7e4deaa7682c0fae2d02c329aadeef167
Marianne Borowiak <marianneb@nulogy.com>
2021-04-26 11:22:24 -0400

    fix example folder for more than one word components

--- src/components/LiveCode/Example.js
+++ src/components/LiveCode/Example.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect } from 'react'
-import {useView, Error } from 'react-view';
+import React, { useState, useEffect } from 'react'
+import { useView, Error } from 'react-view';
 import styled from 'styled-components';
 import Compiler from './Compiler';
 import Editor from './Editor';
-import {Box} from "@nulogy/components";
-import {CODE_WIDTH} from "../CONSTANTS";
+import { Box } from "@nulogy/components";
+import { CODE_WIDTH } from "../CONSTANTS";
 import ActionButtons from './ActionButtons';
 
 const CodeButton = styled.button(({ theme }) => ({
@@ -16,14 +16,14 @@ const CodeButton = styled.button(({ theme }) => ({
 
 const getInitialCode = async (componentName, example) => {
 
-  const codeExample = await import(`!!raw-loader!../../examples/${componentName.toLowerCase()}/${example}.jsx`);
+  const codeExample = await import(`!!raw-loader!../../examples/${componentName.split(/(?=[A-Z])/).join('-').toLowerCase()}/${example}.jsx`);
   if (!codeExample.default || typeof codeExample.default !== 'string') return "";
   return codeExample.default
 };
 
-const Example = ({ componentName, scope, example, ...props}) => {
-  const [ code, setCode ] = useState(null);
-  const [ showCode, setShowCode ] = useState(false);
+const Example = ({ componentName, scope, example, ...props }) => {
+  const [code, setCode] = useState(null);
+  const [showCode, setShowCode] = useState(false);
 
   useEffect(() => {
     if (!code) {
@@ -40,7 +40,7 @@ const Example = ({ componentName, scope, example, ...props}) => {
 
   return code && (
     <Box maxWidth={CODE_WIDTH} m="0 auto">
-      <Compiler {...params.compilerProps}/>
+      <Compiler {...params.compilerProps} />
       { showCode &&
         <>
           <Editor {...params.editorProps} />
@@ -48,7 +48,7 @@ const Example = ({ componentName, scope, example, ...props}) => {
           <ActionButtons {...params.actions} code={params.editorProps.code} componentName={componentName} />
         </>
       }
-      <CodeButton onClick={() => setShowCode(!showCode)}>{ showCode ? "</> Hide Code" :  "</> Show Code"}</CodeButton>
+      <CodeButton onClick={() => setShowCode(!showCode)}>{showCode ? "</> Hide Code" : "</> Show Code"}</CodeButton>
     </Box>
   );
 }
