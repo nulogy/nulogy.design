4330d97ccde9029afe18dffbe1f3eddd367ee400
Marianne Borowiak <marianneb@nulogy.com>
2021-04-27 11:55:28 -0400

    Add animating close button for menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,94 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Box, Branding, List, StatusIndicator, Text, theme } from "@nulogy/components"
 import styled from "styled-components";
 import { navigationLinks } from "../data/navigationLinks";
 import { motion } from "framer-motion";
+
+const Button = styled(Box)(({ theme }) => ({
+  border: "none",
+  background: "none",
+  color: theme.colors.darkBlue,
+  position: "absolute",
+  top: theme.space.x1,
+  left: theme.space.x1,
+  height: "24px",
+  width: "24px"
+}));
+
+
+const MenuButton = ({ isOpen, onClick }) => {
+  const unitWidth = "4"; // width of the lines
+  const variant = isOpen ? "opened" : "closed";
+  const top = {
+    closed: {
+      rotate: 0,
+      translateY: 0
+    },
+    opened: {
+      rotate: 45,
+      translateY: 2
+    }
+  };
+  const center = {
+    closed: {
+      opacity: 1
+    },
+    opened: {
+      opacity: 0
+    }
+  };
+  const bottom = {
+    closed: {
+      rotate: 0,
+      translateY: 0
+    },
+    opened: {
+      rotate: -45,
+      translateY: -2
+    }
+  };
+  const lineProps = {
+    stroke: "darkBlue",
+    strokeWidth: "2",
+    vectorEffect: "non-scaling-stroke",
+    initial: "closed",
+    animate: variant,
+  }
+  return (<Button as="button" display={{ extraSmall: "block", medium: "none" }} onClick={onClick} >
+    <motion.svg
+      viewBox="0 0 4 4"
+      overflow="visible"
+      preserveAspectRatio="none"
+      width="20px"
+      height="20px"
+    >
+      <motion.line
+        x1="0"
+        x2={unitWidth}
+        y1="0"
+        y2="0"
+        variants={top}
+        {...lineProps}
+      />
+      <motion.line
+        x1="0"
+        x2={unitWidth}
+        y1="2"
+        y2="2"
+        variants={center}
+        {...lineProps}
+      />
+      <motion.line
+        x1="0"
+        x2={unitWidth}
+        y1="4"
+        y2="4"
+        variants={bottom}
+        {...lineProps}
+      />
+    </motion.svg>
+  </Button >)
+}
 
 const variants = {
   hover: {
@@ -38,6 +124,7 @@ const Navigation = ({ location }) => {
   const trimSlashes = path => path.replace(/^\/|\/$/g, "");
   const currentPath = trimSlashes(location?.pathname || "");
   const selectedRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     if (selectedRef && selectedRef.current) {
@@ -50,6 +137,7 @@ const Navigation = ({ location }) => {
 
   return (
     <>
+      <MenuButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
       <Box mb="x8" mt="-24px">
         <a style={{ textDecoration: "none" }} href="/" textDecoration="none"><Branding size="large" logoColor="white" withLine mt="x4" mb="x8" subtext="Design System" /></a>
       </Box>
