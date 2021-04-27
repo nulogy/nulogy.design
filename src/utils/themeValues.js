d508d4281e5ce55ad7bfc680b42b1dd78e442c63
Matt Dunn <mattd@nulogy.com>
2021-04-27 13:40:17 -0400

    export theme values from a common place

---
+++ src/utils/themeValues.js
@@ -0,0 +1,4 @@
+export const COLOURS = ["black", "blackBlue", "darkBlue", "blue", "lightBlue", "darkGrey", "grey", "whiteGrey", "white", "yellow", "green", "lightGreen", "red", "lightRed"]
+export const SIZES = ["x1", "x2", "x3", "x4", "x6", "x8"]
+export const SHADOWS = ["small", "medium", "large", "focus", "error"]
+export const RADII = ["small", "medium", "circle"]
