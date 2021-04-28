3acc06822845b754a755f30e1fcbaefa184397b9
Marianne Borowiak <marianneb@nulogy.com>
2021-04-28 15:29:35 -0400

    get token values from theme

--- src/utils/themeValues.js
+++ src/utils/themeValues.js
@@ -1,4 +1,6 @@
-export const COLOURS = ["black", "blackBlue", "darkBlue", "blue", "lightBlue", "darkGrey", "grey", "whiteGrey", "white", "yellow", "green", "lightGreen", "red", "lightRed"]
-export const SIZES = ["x1", "x2", "x3", "x4", "x6", "x8"]
-export const SHADOWS = ["small", "medium", "large", "focus", "error"]
-export const RADII = ["small", "medium", "circle"]
+import { theme } from "@nulogy/components"
+
+export const COLOURS = Object.keys(theme.colors)
+export const SIZES = Object.keys(theme.space)
+export const SHADOWS = Object.keys(theme.shadows)
+export const RADII = Object.keys(theme.radii)
